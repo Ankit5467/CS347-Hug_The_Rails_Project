ae485c266e64bcead7a7244410fda814a1fe3ef5
Ankit Patel <ankit.patel.1701.5467@gmail.com>
2021-04-17 13:20:25 -0400

    Create testClass.java

---
+++ HTRJavaProject/testClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ankit
+ *
+ */
+public class testClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
