a9f423a1216fe1350f9d2c15d918f38b120270fe
Ankit Patel <ankit.patel.1701.5467@gmail.com>
2021-04-24 18:55:18 -0400

    Added rounding
    - Most numerical outputs are now rounded to 1 or 2 decimal places.

--- HTRJavaProject/htrPackage/IOT.java
+++ HTRJavaProject/htrPackage/IOT.java
@@ -10,8 +10,12 @@ public class IOT extends Sensors {
 		super();
 	}
 
+	static double roundTwoDecimals(double db) {
+		return ((double) ((int)(db*100))) / 100;
+	}
+
 	String windReport() {
-		String data = "The wind speed is " + this.getWindSpeed() + " mph. ";
+		String data = "The wind speed is " + roundTwoDecimals(this.getWindSpeed()) + " mph. ";
 		String rec = "";
 		if (this.getWindSpeed() >= 50) {
 			rec += "Recommendation: Reduce the speed of the train.";
@@ -20,18 +24,18 @@ public class IOT extends Sensors {
 	}
 
 	String rainReport() {
-		String data = "The rate of rainfall is " + this.getRainRate() + " inches per hour.";
+		String data = "The rate of rainfall is " + roundTwoDecimals(this.getRainRate()) + " inches per hour.";
 		String rec = "";
 		if (this.getRainRate() >= 0.3) {
 			rec += " Recommendation: Turn on the train headlights and reduce the speed of the train.";
-			return "The rate of rainfall is " + this.getRainRate() + " inches per hour.\n"
+			return "The rate of rainfall is " + roundTwoDecimals(this.getRainRate()) + " inches per hour.\n"
 					+ "Recommendation: Turn on the train headlights and reduce the speed of the train.";
 		}
 		return data + rec;
 	}
 
 	String snowReport() {
-		String data = "The rate of snowfall is " + this.getSnowRate() + " inches per hour.";
+		String data = "The rate of snowfall is " + roundTwoDecimals(this.getSnowRate()) + " inches per hour.";
 		String rec = "";
 		if (this.getSnowRate() >= 0.3) {
 			rec += " Recommendation: Turn on the train headlights and reduce the speed of the train.";
@@ -41,7 +45,7 @@ public class IOT extends Sensors {
 
 	String visibilityReport() {
 		if (this.getVisibility() < 2.0) {
-			return "The visibility is " + this.getVisibility() + " miles. Recommendation: Turn on the train"
+			return "The visibility is " + roundTwoDecimals(this.getVisibility()) + " miles. Recommendation: Turn on the train"
 					+ " headlights and reduce the speed of the train.";
 		} else {
 			return "The visibility is at least 2 miles.";
@@ -52,7 +56,7 @@ public class IOT extends Sensors {
 		// print the weather report.
 		String precipitation = "";
 		if (this.getRainRate() < 0.3 && this.getSnowRate() < 0.3) {
-			precipitation += "The rate of precipitation is " + (this.getRainRate() + this.getSnowRate())
+			precipitation += "The rate of precipitation is " + roundTwoDecimals(this.getRainRate() + this.getSnowRate())
 					+ " inches per hour.";
 			return this.windReport() + "\n" + precipitation + "\n" + visibilityReport();
 		} else {
@@ -80,7 +84,8 @@ public class IOT extends Sensors {
 	 */
 	double objectSpeed() {
 		// get 2 distance measurements.
-		return ((double) ((int) ((1 + (Math.random() * ((15 - 1) + 1))) * 100))) / 100; /* generate random speed b/w [1,15] */
+		// return ((double) ((int) ((1 + (Math.random() * ((15 - 1) + 1))) * 100))) / 100; /* generate random speed b/w [1,15] */
+		return (1 + (Math.random() * ((15 - 1) + 1)));
 	}
 
 	/**
@@ -89,7 +94,8 @@ public class IOT extends Sensors {
 	 */
 	double computeImpact() {
 		// returns time to impact.
-		return ((double) ((int)((this.obtainDistanceFromObject() / (88 * this.getSpeed())) * 100))) / 100;
+		// return ((double) ((int)((this.obtainDistanceFromObject() / (88 * this.getSpeed())) * 100))) / 100;
+		return ((this.obtainDistanceFromObject() / (88 * this.getSpeed())));
 	}
 
 	void ProcessObject() {
@@ -99,22 +105,22 @@ public class IOT extends Sensors {
 		} else {
 			if (this.obtainDistanceFromObject() < 0) { /* obstruction behind train */
 				System.out.println(
-						"There is an obstruction " + this.obtainDistanceFromObject() + " feet behind the train.");
+						"There is an obstruction " + roundTwoDecimals(this.obtainDistanceFromObject()) + " feet behind the train.");
 				if (this.getDetectMovingObject()) {
-					System.out.println("Obstruction is moving. It's speed is " + this.objectSpeed() + " mph.");
+					System.out.println("Obstruction is moving. It's speed is " + roundTwoDecimals(this.objectSpeed()) + " mph.");
 				} else {
 					System.out.println("Obstruction is stationary.");
 				}
 
 			} else { /* obstruction in front of train */
-				System.out.println("There is an obstruction " + this.obtainDistanceFromObject()
+				System.out.println("There is an obstruction " + roundTwoDecimals(this.obtainDistanceFromObject())
 						+ " feet in front of the train.");
 				if (this.getDetectMovingObject()) {
-					System.out.println("Obstruction is moving. It's speed is " + this.objectSpeed() + " mph.");
+					System.out.println("Obstruction is moving. It's speed is " + roundTwoDecimals(this.objectSpeed()) + " mph.");
 				} else {
 					System.out.println("Obstruction is stationary.");
 				}
-				System.out.println("Estimated time to impact: " + this.computeImpact() + " minutes.");
+				System.out.println("Estimated time to impact: " + roundTwoDecimals(this.computeImpact()) + " minutes.");
 			}
 		}
 
@@ -146,15 +152,15 @@ public class IOT extends Sensors {
 		// true if gate is open, false if closed.
 		if (getGateDistance() > 2.0) {
 			if (this.getGateStatus()) {
-				return "The next gate, which is " + getGateDistance() + " miles away, is open.";
+				return "The next gate, which is " + roundTwoDecimals(getGateDistance()) + " miles away, is open.";
 			} else {
-				return "The next gate, which is " + getGateDistance() + " miles away, is closed.";
+				return "The next gate, which is " + roundTwoDecimals(getGateDistance()) + " miles away, is closed.";
 			}
 		} else {
 			if (getGateStatus()) {
-				return "The next gate, which is " + getGateDistance() + " miles away, is open.";
+				return "The next gate, which is " + roundTwoDecimals(getGateDistance()) + " miles away, is open.";
 			} else {
-				return "The next gate, which is " + getGateDistance() + " miles away, is closed.\n"
+				return "The next gate, which is " + roundTwoDecimals(getGateDistance()) + " miles away, is closed.\n"
 						+ " Recommendation: Stop the train immediately & wait for the gate to open.";
 			}
 		}
@@ -165,7 +171,7 @@ public class IOT extends Sensors {
 		return "help Message:\n" + "\tEnter \"help\" to display the help message\n" + "\tEnter \"exit\" to exit LCS\n"
 				+ "\tEnter \"log off\" to log out of LCS\n"
 				+ "\tEnter \"view log\" to view the log. (Only available for the operator.\n" + "\tWheel Diamater: "
-				+ this.getWheelDiameter() + " inches.\n" + "Command Options:\n"
+				+ roundTwoDecimals(this.getWheelDiameter()) + " inches.\n" + "Command Options:\n"
 				+ "\twifi \n\tadd user \n\tlocation \n\tweather \n\tspeed"
 				+ "\n\trpm \n\trecommend \n\tstatus \n\tset diameter";
 	}
