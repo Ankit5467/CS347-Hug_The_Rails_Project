e91d6ba8fdb4641e543e385360d87c689386fec8
farpath20 <55555284+farpath20@users.noreply.github.com>
2021-04-24 17:39:11 -0400

    Update Sensors.java

--- HTRJavaProject/htrPackage/Sensors.java
+++ HTRJavaProject/htrPackage/Sensors.java
@@ -1,4 +1,5 @@
 package htrPackage;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -7,69 +8,73 @@ import java.util.ArrayList;
 public class Sensors {
 
 	/* hardware: */
-	
-	public double wheel_diameter;	/** Wheel diameter in inches **/
-	
+
+	public double wheel_diameter;
+	/** Wheel diameter in inches **/
+
 	/* Data fields: */
 
 	private double longitude;
 	private double latitude;
-	 
+
 	private double gate_distance;
 	private boolean gate_status;
 
 	private boolean moving_obstruction;
 	private boolean stationary_obstruction;
 	private double distance_from_obstruction;
-	
+
 	private int rpm;
-	
+
 	private double speed;
 	private double wind_speed;
-	
+
 	private double rate_rain;
 	private double rate_snow;
-	
+
 	private double visibility;
 	protected Scanner docc;
-	private ArrayList <String> data;
+	private ArrayList<String> data;
 
 	/**
 	 * constructor for Sensor object.
 	 */
 	protected Sensors() {
 		/* Initialize the sensor object with the following default values */
-		//TODO: Chaßnges these values to 0 after testing.
-		/*this.wheel_diameter = 40.0;
-		this.longitude = 1.0000;
-		this.latitude = 1.0000;
-		this.gate_distance = 1.0;
+		// TODO: Change these values to 0 after testing.
+		/*
+		 * this.wheel_diameter = 40.0; this.longitude = 1.0000; this.latitude = 1.0000;
+		 * this.gate_distance = 1.0; /* miles
+		 */
 		this.gate_status = false;
-		this.moving_obstruction = false;
+		this.moving_obstruction = true;
 		this.stationary_obstruction = false;
-		this.distance_from_obstruction = 2.0;
+		this.distance_from_obstruction = 1200; /* feet */
 		this.rpm = 100;
-		this.speed = 10.0;
-		this.wind_speed = 100.0;
-		this.rate_rain = 1.0;
-		this.rate_snow = 0.2;
-		this.visibility = 2.0;*/
-	
-		
-	}
-	 public void updateValues()
-	{
-		data = new ArrayList<String>();
-		
-		try {
-          
+		this.speed = 50.0; /* mph */
+		this.wind_speed = 100.0; /* mph */
+		this.rate_rain = 1.0; /* inches per hour */
+		this.rate_snow = 0.2; /* inches per hour */
+		this.visibility = 2.0; /* miles */
 
-            Scanner input = new Scanner(System.in);
+	}
+
+	public void updateValues() {
+		data = new ArrayList<String>();
+
+		try {
+
+			Scanner input = new Scanner(System.in);
 
             File file = new File("poop.txt");
 
-            input = new Scanner(file);
+			input = new Scanner(file);
 
+			while (input.hasNextLine()) {
+				String line = input.nextLine();
+				System.out.println(line);
+			}
+			input.close();
 
             while (input.hasNextLine()) {
                 String line = input.nextLine();
@@ -99,14 +104,28 @@ public class Sensors {
 
 		
 	}
+	// void updateValues()
+	// {
+	//
+	//
+	//
+	// }
+	// void testDoc()
+	// {
+	// for(int i = 0; i<data.size();i++)
+	// {
+	// System.out.println(data.get(i));
+	// }
+	// }
 
 	/**
 	 * sets the wheel diameter to @param diameter.
+	 * 
 	 * @return 0 on success. -1 on failure.
 	 */
 	int setWheelDiameter(double diameter) {
 		if (diameter < 1.0) {
-//			System.out.println("Error: Diameter must be at least 1 inch!");
+			// System.out.println("Error: Diameter must be at least 1 inch!");
 			return -1;
 		} else {
 			this.wheel_diameter = diameter;
@@ -118,23 +137,22 @@ public class Sensors {
 	 * @return the wheel diameter (in inches).
 	 */
 	double getWheelDiameter() {
-		return this.wheel_diameter;		/* use randomizer only once? */
+		return this.wheel_diameter; /* use randomizer only once? */
 	}
-	
+
 	/**
-	 * sets the longitude & latitude.
-	 * Returns 0 on success, -1 on failure.
+	 * sets the longitude & latitude. Returns 0 on success, -1 on failure.
 	 */
 	int setLocation(double latitude, double longitude) {
 		if (Math.abs(latitude) > 90 || longitude < 0 || longitude > 180) {
 			System.out.println("Error: Invalid coordinates");
 			return -1;
 		}
-		this.longitude = longitude;	/* use randomizer */
-		this.latitude = latitude;	/* use randomizer */
+		this.longitude = longitude; /* use randomizer */
+		this.latitude = latitude; /* use randomizer */
 		return 0;
 	}
-	
+
 	/**
 	 * @return the latitude.
 	 */
@@ -148,16 +166,15 @@ public class Sensors {
 	double getLongitude() {
 		return this.longitude;
 	}
-	
+
 	/**
-	 * Set the distance to the next gate.
-	 * Returns 0 on success and -1 on failure.
+	 * Set the distance to the next gate. Returns 0 on success and -1 on failure.
 	 */
 	int setGateDistance(double dist) {
 		if (dist > 0 || dist < 250.0) {
 			return -1;
 		}
-		this.gate_distance = dist;	/* use randomizer */
+		this.gate_distance = dist; /* use randomizer */
 		return 0;
 	}
 
@@ -167,10 +184,9 @@ public class Sensors {
 	double getGateDistance() {
 		return this.gate_distance;
 	}
-	
+
 	/**
-	 * Set the gate_status data field to true if next gate is open,
-	 * 	otherwise false.
+	 * Set the gate_status data field to true if next gate is open, otherwise false.
 	 */
 	void setGateStatus() {
 		this.gate_status = true; /* use randomizer */
@@ -184,8 +200,8 @@ public class Sensors {
 	}
 
 	/**
-	 * If a stationary object is detected, set the data field
-	 * 	 stationary_obstruction to true, otherwise false.
+	 * If a stationary object is detected, set the data field stationary_obstruction
+	 * to true, otherwise false.
 	 */
 	void setDetectStationaryObject() {
 		this.stationary_obstruction = true; /* use randomizer */
@@ -197,10 +213,10 @@ public class Sensors {
 	boolean getDetectStationaryObject() {
 		return this.stationary_obstruction;
 	}
-	
+
 	/**
-	 * If a moving object is detected, set the data field
-	 * 	 moving_obstruction to true, otherwise false.
+	 * If a moving object is detected, set the data field moving_obstruction to
+	 * true, otherwise false.
 	 */
 	void setDetectMovingObject() {
 		this.moving_obstruction = true; /* use randomizer */
@@ -212,31 +228,31 @@ public class Sensors {
 	boolean getDetectMovingObject() {
 		return this.moving_obstruction;
 	}
-	
+
 	// TODO
 	/**
 	 * Sets then gets distance from object.
+	 * 
 	 * @return a negative double to signify obstruction's distance behind the train,
-	 * 	and a non-negative double to signify distance in front of the train.
+	 *         and a non-negative double to signify distance in front of the train.
 	 */
 	double obtainDistanceFromObject() {
-		//distance b/w train and obstacle.
-		double dist = -1.0;
-		if(getDetectStationaryObject()) {
-			//some math
-			dist = 2.0;
-		}
-		if(getDetectMovingObject()) {
-			//some math
-			dist = 3.0;
-		}
-		this.distance_from_obstruction = dist;
+		// distance b/w train and obstacle.
+		// double dist = -1.0;
+		// if(getDetectStationaryObject()) {
+		// //some math
+		// dist = 2.0;
+		// }
+		// if(getDetectMovingObject()) {
+		// //some math
+		// dist = 3.0;
+		// }
+		// this.distance_from_obstruction = dist;
 		return this.distance_from_obstruction;
 	}
-	
+
 	/**
-	 * Set the rpm.
-	 * Return 0 on success & -1 on failure.
+	 * Set the rpm. Return 0 on success & -1 on failure.
 	 */
 	int setRPM(int rpm) {
 		if (rpm < 0 || rpm > 5000) {
@@ -247,15 +263,14 @@ public class Sensors {
 	}
 
 	/**
-	 * @return the rpm. 
+	 * @return the rpm.
 	 */
 	int getRPM() {
 		return this.rpm;
 	}
-	
+
 	/**
-	 * Set the speed.
-	 * Return 0 on success & -1 on failure.
+	 * Set the speed. Return 0 on success & -1 on failure.
 	 */
 	int setSpeed(double speed) {
 		if (speed < 0.0 || speed > 400.0) {
@@ -266,15 +281,14 @@ public class Sensors {
 	}
 
 	/**
-	 * @return the speed in miles per hour. 
+	 * @return the speed in miles per hour.
 	 */
 	double getSpeed() {
 		return this.speed;
 	}
-	
+
 	/**
-	 * Set the speed.
-	 * Return 0 on success & -1 on failure.
+	 * Set the speed. Return 0 on success & -1 on failure.
 	 */
 	int setWindSpeed(double wind_speed) {
 		if (wind_speed < 0 || wind_speed > 400) {
@@ -285,15 +299,14 @@ public class Sensors {
 	}
 
 	/**
-	 * @return the wind speed (miles per hour). 
+	 * @return the wind speed (miles per hour).
 	 */
 	double getWindSpeed() {
 		return this.wind_speed;
 	}
-	
+
 	/**
-	 * Set the rain rate.
-	 * Return 0 on success & -1 on failure.
+	 * Set the rain rate. Return 0 on success & -1 on failure.
 	 */
 	int setRainRate(double rate) {
 		if (rate < 0 || rate > 6) {
@@ -304,15 +317,14 @@ public class Sensors {
 	}
 
 	/**
-	 * @return the rain rate (inches per hour). 
+	 * @return the rain rate (inches per hour).
 	 */
 	double getRainRate() {
 		return this.rate_rain;
 	}
-	
+
 	/**
-	 * Set the snow rate.
-	 * Return 0 on success & -1 on failure.
+	 * Set the snow rate. Return 0 on success & -1 on failure.
 	 */
 	int setSnowRate(double rate) {
 		if (rate < 0 || rate > 6) {
@@ -323,15 +335,14 @@ public class Sensors {
 	}
 
 	/**
-	 * @return the snow rate (inches per hour). 
+	 * @return the snow rate (inches per hour).
 	 */
 	double getSnowRate() {
 		return this.rate_snow;
 	}
-	
+
 	/**
-	 * set the visibility.
-	 * Return 0 on success & -1 on failure.
+	 * set the visibility. Return 0 on success & -1 on failure.
 	 */
 	int setVisibility(double visibility) {
 		if (visibility < 0 || visibility > 2.0) {
@@ -342,18 +353,18 @@ public class Sensors {
 	}
 
 	/**
-	 * @return the visibility (in miles).
-	 * 	Range of visibility is b/w 0.0 & 2.0 miles inclusive.
+	 * @return the visibility (in miles). Range of visibility is b/w 0.0 & 2.0 miles
+	 *         inclusive.
 	 */
 	double getVisibility() {
-		return this.visibility;	
+		return this.visibility;
 	}
-	public static void main(String[]args)
-	{
+
+	public static void main(String[] args) {
 		Sensors s = new Sensors();
 		System.out.println("kpppp");
 		s.updateValues();
 		
 	}
-	
+
 }
